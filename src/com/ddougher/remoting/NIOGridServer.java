53a58c2ea21d3772fd2d147555fa912fceaf624f
Dana Dougherty <danaanne1@gmail.com>
2021-12-15 22:30:03 -0600

    checkpoint commit

--- src/com/ddougher/remoting/NIOGridServer.java
+++ src/com/ddougher/remoting/NIOGridServer.java
@@ -8,15 +8,19 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.SocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -26,25 +30,25 @@ import com.ddougher.remoting.GridProtocol.FindClassResponse;
 import com.theunknowablebits.proxamic.TimeBasedUUIDGenerator;
 
 /**
- * Parallel Processing is the new frontier. Doing it smoothly, transparently, and seamlessly... is the vehicle that will get us across.
- * <p>
- * This is not your grandmas task/job execution engine based pile of crap. This is real software created by real geniuses for
- * accomplishing real things.
- * <p>
+ * NIO Remote Object Server with ClassLoading capability.
  * 
  * @author Dana
- *
  */
 public class NIOGridServer {
 
-	ServerSocketChannel serverChannel;
+	private ServerSocketChannel serverChannel;
 	private Selector selector;
 	private ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
 	private volatile boolean closed = false;
+	private ConcurrentLinkedDeque<ByteBuffer> bufferPool = new ConcurrentLinkedDeque<ByteBuffer>();
+	
 	
 	private class Context {
 		GridProxiedClassLoader gpcl;
-		Map<UUID,Object> localObjects = new HashMap<UUID, Object>();
+		Map<UUID,Object> instances = new HashMap<UUID, Object>();
+		LinkedList<ByteBuffer> incoming = new LinkedList<>(Collections.singleton(ByteBuffer.allocateDirect(65535)));
+		LinkedList<ByteBuffer> outgoing = new LinkedList<>();
+		ClassLoader classloader;
 	}
 
 	public NIOGridServer(SocketAddress address) throws IOException {
@@ -79,67 +83,117 @@ public class NIOGridServer {
 				}
 			} catch (IOException ioe) {
 				ioe.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 	}
 	
 	// Roll in bytes until we can respond
-	void read(SelectionKey key, SocketChannel channel, Context context) {
-		
+	void read(SelectionKey key, SocketChannel channel, Context context) throws IOException, ClassNotFoundException {
+		ByteBuffer buffer;
+		int c=1;
+		while (c>0) {
+			if (context.incoming.isEmpty() || !context.incoming.getLast().hasRemaining()) 
+				context.incoming.add(allocateBuffer());
+			c = channel.read(context.incoming.getLast());
+			decantReadyMessage(context);
+		}
 	}
+	
+	ByteBuffer allocateBuffer() {
+		ByteBuffer buffer;
+		if (null==(buffer=bufferPool.poll())) buffer = ByteBuffer.allocateDirect(65535);
+		return buffer;
+	}
+	
+	void decantReadyMessage(Context context) throws IOException, ClassNotFoundException {
+		long totalLength = context.incoming.stream().reduce(0L,(val,buf)->val+buf.position(),(a,b)->a+b);
+		Object result;
+		if (totalLength > 8) {
+			long messageLength = context.incoming.getFirst().getLong(0);
+			if (totalLength >= messageLength) {
+				try (	InputStream gin = (InputStream)new DecantingInputStream(messageLength,context.incoming);
+						ObjectInputStream oin = new ObjectInputStream(gin); ) 
+				{
+					result = oin.readObject();
+				}
+				long remainingLength = messageLength;
+				while (remainingLength > 0) {
+					ByteBuffer buffer = context.incoming.peekFirst();
+					if (buffer.position()<remainingLength) {
+						remainingLength -= buffer.position();
+						bufferPool.add((ByteBuffer)context.incoming.poll().clear());
+						continue;
+					} 
+					buffer.flip();
+					buffer.position((int)remainingLength);
+					buffer.compact();
+					if (buffer.position()<=0) 
+						bufferPool.add((ByteBuffer)context.incoming.poll().clear());
+					remainingLength = 0;
+				}
+				dispatchMessage(context, result);
+			}
+		}
+	}
+
+	private void dispatchMessage(Context context, Object result) {
+		cachedThreadPool.submit(
+			()->
+				// find and execute the function matching the type of the input object
+				this
+				.getClass()
+				.getMethod("execute", result.getClass(), Context.class)
+				.invoke(this, result, context));
+	}
+	
+	private class DecantingInputStream extends InputStream {
+
+		long messageLength;
+		LinkedList<ByteBuffer> incoming;
+
+		public DecantingInputStream(long messageLength, LinkedList<ByteBuffer> incoming) {
+			super();
+			this.messageLength = messageLength;
+			this.incoming = incoming;
+		}
+
+		@Override
+		public int read() throws IOException {
+			byte [] b = new byte[1];
+			read(b);
+			return b[0];
+		}
+		
+		@Override
+		public int read(byte[] b) throws IOException {
+			return read(b,0,b.length);
+		}
+		
+		@Override
+		public int read(byte[] b, int off, int len) throws IOException {
+
+			return 0;
+		}
+	}
+	
 	
 	// Roll out bytes from the outgoing queue. If the queue is empty, change interest to readOnly
 	void write(SelectionKey key, SocketChannel channel, Context context) {
 		
 	}
 	
-
-	void respond(Context context) throws IOException {
-		try (
-				InputStream cin = Channels.newInputStream(channel);
-				BufferedInputStream bin = new BufferedInputStream(cin);
-				ObjectInputStream oin = new ObjectInputStream(bin);
-				OutputStream cout = Channels.newOutputStream(channel);
-				BufferedOutputStream bout = new BufferedOutputStream(cout);
-				ObjectOutputStream oout = new ObjectOutputStream(bout)   )
-		{
-			Map<UUID,Object> objects = new ConcurrentHashMap<>();
-			GridProxiedClassLoader classLoader = new GridProxiedClassLoader(oout);
-			while (true) {
-				readIncomingRequest(oin, oout, objects, classLoader);
-			}
-		} finally {
-			channel.close();
-		}
-	}
-
-	void readIncomingRequest(ObjectInputStream in, ObjectOutputStream out, Map<UUID,Object> objects, GridProxiedClassLoader classLoader) throws IOException  {
-		try {
-			Object ob = in.readObject();
-			cachedThreadPool.submit(
-				()->
-					// find and execute the function matching the type of the input object
-					this
-					.getClass()
-					.getMethod("execute", ob.getClass(), ObjectOutputStream.class, Map.class, GridProxiedClassLoader.class)
-					.invoke(this, ob, out, objects, classLoader));
-		} catch (ReflectiveOperationException  e) {
-			e.printStackTrace(System.err);
-		}
-	}
-	
-	void execute(CreateObjectRequest req, ObjectOutputStream out, Map<UUID, Object> objects, GridProxiedClassLoader classLoader) throws Exception  {
+	void execute(CreateObjectRequest req, Context context) throws Exception  {
 		UUID u = TimeBasedUUIDGenerator.instance().nextUUID();
-		Object result = classLoader.loadClass(req.className).getConstructor(req.parameters).newInstance((Object [])req.args);
-		objects.put(u, result);
-		synchronized(out) {
-			out.writeObject(new CreateObjectResponse(req.requestId,u));
-			out.flush();
-		}
+		Object result = context.classloader.loadClass(req.className).getConstructor(req.parameters).newInstance((Object [])req.args);
+		context.instances.put(u, result);
+		writeToContext(new CreateObjectResponse(req.requestId,u));
 	}
 
-	void execute(FindClassResponse resp, ObjectOutputStream out, Map<UUID,Object> objects, GridProxiedClassLoader classLoader) throws Exception {
-		classLoader.handleFindClassResponse(resp);
+	void execute(FindClassResponse resp, Context context) throws Exception {
+		context.classloader.handleFindClassResponse(resp);
 	}
 
 	interface Invocation {
