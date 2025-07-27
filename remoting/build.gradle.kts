b2e011f6970adbd2c16a93d91eb33b79f8146afc
Dana Dougherty <danaanne1@gmail.com>
2025-07-27 10:39:16 -0700

    eliminate kotlin from remoting build

--- remoting/build.gradle.kts
+++ remoting/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     java
-    kotlin("jvm")
 }
 
 group = "com.ddougher"
@@ -15,18 +14,12 @@ sourceSets {
         java {
             srcDir("src")
         }
-        kotlin {
-            srcDir("src")
-        }
         resources {
             srcDir("src")
         }
     }
     test {
         java {
-            srcDir("tst")
-        }
-        kotlin {
             srcDir("tst")
         }
         resources {
@@ -36,23 +29,15 @@ sourceSets {
 }
 
 dependencies {
-    // Kotlin
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.21")
-    
     // Testing
     testImplementation(project(":"))  // Main project dependency for tests
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.21")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 }
 
-// Set Java compatibility to 1.8 to match Kotlin target
+// Set Java compatibility to 1.8
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
 }
 
 tasks.withType<Test> {
